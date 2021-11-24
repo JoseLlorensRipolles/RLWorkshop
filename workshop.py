c93c5f07ef688db45dcd13e42200e61ee006af88
José Manuel Llorens Ripollés <jose.llorens.ripolles@gmail.com>
2021-11-24 11:49:07 +0100

    Cartpole env

--- workshop.py
+++ workshop.py
@@ -2,4 +2,18 @@ import gym
 import torch
 import numpy as np
 
-print("Ready")
+EPISODES = 100
+
+if __name__ == "__main__":
+
+    env = gym.make('CartPole-v1')
+    for episode in range(EPISODES):
+        env.reset()
+        done = False
+        total_reward = 0
+        while not done:
+            env.render()
+            _, r, done, _ = env.step(env.action_space.sample())
+            total_reward += r
+        print(f"Total reward for episode #{episode}: {total_reward}")
+    env.close()
